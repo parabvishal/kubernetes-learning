8d4c35136c70fe6d2b338a82531d4826473e3054
Parab <vishal.parab@abbott.com>
2020-11-14 20:45:29 +0530

    updated python scripts

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ RUN pip install gunicorn flask
 ADD mainapp.py wsgi.py /app/
 EXPOSE 5000
 WORKDIR /app
-ENTRYPOINT [ "gunicorn","--bind","0.0.0.0:5000","msgi:app" ]
+ENTRYPOINT [ "gunicorn","--bind","0.0.0.0:5000","wsgi:app" ]
--- mainapp.py
+++ mainapp.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    response = {"message":"Hello World!"}
-    return jsonify(response)
+   response = {"message":"Hello World!"}
+   return jsonify(response)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+   app.run(host='0.0.0.0')
